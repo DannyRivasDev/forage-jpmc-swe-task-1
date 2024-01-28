e5b299ce994da04d1f2bda69cbdc970d13740a6e
Danny <dannyrivasdev@gmail.com>
2024-01-28 01:06:18 -0500

    Update

--- client3.py
+++ client3.py
@@ -28,7 +28,6 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
-
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
@@ -38,14 +37,12 @@ def getDataPoint(quote):
     price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
-
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     if price_b == 0:
         return
     return price_a / price_b
-
 
 # Main
 if __name__ == "__main__":
